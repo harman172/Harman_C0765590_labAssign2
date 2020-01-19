d29bb4908a97cec635696fb84a7faf3ce4185738
Harman Kaur <kaurharmanpreet502@gmail.com>
2020-01-19 18:50:14 -0500

    Initial Commit

---
+++ Harman_C0765590_labAssign2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Harman_C0765590_labAssign2
+//
+//  Created by Harmanpreet Kaur on 2020-01-19.
+//  Copyright © 2020 Harmanpreet Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
